ed2236af212a8e8b193a56c52f549cf72cf2efe9
Viprex <viprex82@gmail.com>
2015-07-27 10:35:36 +0200

    First test file

---
+++ Exchange_SendAs_ShowDisabledUser_V1.0.ps1
@@ -0,0 +1,6 @@
+﻿# Get-Mailbox -Resultsize Unlimited | Get-ADPermission | Where {($_.ExtendedRights -like "*Send-As*") -and ($_.User -ne "*NT-AUTORITÄT\SELBST*") -and ($_.User -ne "*BOSKAMP\Domänen-Admins*") -and ($_.User -ne "*BOSKAMP\APP-Exchange-SendenAls-Berechtigung*")} | select Identity,User | export-csv C:\_Skripting-Ausgabe\Exchange_SendAs_ShowDisabledUser.csv -Delimiter ";" -encoding unicode
+Get-Mailbox -Resultsize Unlimited | Get-ADPermission | Where {($_.ExtendedRights -like "*Send-As*")} | select Identity,User
+
+
+
+#export-csv C:\_Skripting-Ausgabe\Exchange_SendAs_ShowDisabledUser.csv -Delimiter ";" -encoding unicode
