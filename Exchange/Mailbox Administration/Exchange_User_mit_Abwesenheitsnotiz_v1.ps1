bd8802fb7e6c654684d4212d0d0801b07ee3ed78
Viprex <viprex82@gmail.com>
2015-07-27 10:41:02 +0200

    Version2

--- Exchange/Mailbox Administration/Exchange_User_mit_Abwesenheitsnotiz_v1.ps1
+++ Exchange/Mailbox Administration/Exchange_User_mit_Abwesenheitsnotiz_v1.ps1
@@ -1,1 +1,22 @@
-Get-Mailbox | Get-MailboxAutoReplyConfiguration | Where-Object { $_.AutoReplyState -eq "Enabled" -or $_.AutoReplyState -eq "scheduled" } | fl identity
+$Postfach = Get-Mailbox 
+$Postfach | % {
+	$displayname = $_.displayname
+	$now=Get-Date
+	Get-MailboxAutoReplyConfiguration $_ | ? { ($_.AutoReplyState -ne "disabled") -and ($_.starttime -lt $now) -and ($_.endtime -gt $now) } | 
+	% {
+		$OOFText=$_.internalmessage
+			
+		if ($_.AutoReplyState -eq "enabled")
+		{ 
+			Write-Host $displayname " ===> aktuell abwesend"
+			#$OOFText | Out-File -FilePath "C:\temp\$displayname.txt" -encoding "default"
+		}
+		else
+		{		
+			Write-Host $displayname "geplant von " $_.starttime " bis " $_.endtime
+			#"abwesend von:" | Out-File -FilePath "C:\temp\$displayname.txt" -encoding "default" 
+			#$_ | Select starttime,endtime | Out-File -FilePath "C:\temp\$displayname.txt" -encoding "default" -append 
+			#$OOFText | Out-File -FilePath "C:\temp\$displayname.txt" -encoding "default" -append
+		}		
+	}
+}
